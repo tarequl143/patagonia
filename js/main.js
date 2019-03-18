69286bbbc54ab3e370911a435a7fc7e2383ac475
tarequl <tarequl143@gmail.com>
2019-03-18 23:18:37 +0600

    Artcenter Page added

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@ jQuery(function ($) {
 
     "use strict"
 
-    /*====================================
-    Scroll Top Script ====================
-    ====================================*/
-    $(".scroll-top").on("click", function () { 
-        var scrollTop = $(this).attr("href");
-        $("html, body").animate({
-            scrollTop: $(scrollTop).offset().top
-        }, 1000);
-        return false;
-    });
-
     /*--===== Mobile Menu =====--*/
     var MenuContent = $(".main-menu").html();
     $(".mobile-menu").html(MenuContent);
@@ -64,10 +53,48 @@ jQuery(function ($) {
     });
 
     /*--===== End Slide Boxes Script =====--*/
-    
+
     $('#file').change(function () {
         var filename = $('#file')[0].files[0]['name'];
         $('.file-name').html(filename);
+    });
+
+    $(".dac-patagonia-slider").slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        nextArrow: '.right',
+        prevArrow: '.left',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+    },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+    },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
     });
 
 
